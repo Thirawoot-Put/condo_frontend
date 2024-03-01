6bdeb71411efa3fce50ce0dff74eeb70e5481a83
BoySarut <secendary2@gmail.com>
2024-03-01 16:07:09 +0700

    add-image-slide-hero

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from '../features/homepage/components/Hero';
 import FeaturedList from '../features/homepage/components/FeaturedList';
+import ImageSlide from '../components/ImageSlide';
 
 function HomePage() {
   return (
     <div>
       <Hero />
+      <ImageSlide />
       <FeaturedList />
       <div>popular area</div>
       <div>promote with us</div>
