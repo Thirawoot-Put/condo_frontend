0a460c36f3f40e33f7682524db19cf71d01bbbd5
Thirawoot-put <thirawoot.put@gmail.com>
2024-03-01 16:53:21 +0700

    fixed-conflict

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import Hero from '../features/homepage/components/Hero';
 import FeaturedList from '../features/homepage/components/FeaturedList';
-<<<<<<< HEAD
 import PopularArea from '../features/homepage/components/PopularArea';
 import PromoteWithUs from '../features/homepage/components/PromoteWithUs';
-=======
 import ImageSlide from '../components/ImageSlide';
->>>>>>> develop
 
 function HomePage() {
   return (
