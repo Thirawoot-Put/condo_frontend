890c662d9f9f6a7fe292b1593febab8afac89176
fee-hanafee <hanafee.pass@gmail.com>
2024-03-05 19:11:23 +0700

    75percent

--- src/features/posts_detail/Container.jsx
+++ src/features/posts_detail/Container.jsx
@@ -7,10 +7,16 @@ import DetailRight from './component/DetailRight';
 import Spinner from '../../components/Spinner';
 import ProfileCard from '../../components/ProfileCard';
 import { Button } from '@mui/material';
-
+import MapDisplayOnePin from '../../components/MapDisplayOnePin';
+import Facilities from './component/Facilities';
 
 export default function Container() {
   const { loading, postDetail } = useDetail();
+  const latlong = {
+    lat: +postDetail?.room?.condo?.lat,
+    lng: +postDetail?.room?.condo?.long,
+  };
+  console.log(postDetail);
 
   return (
     <>
@@ -19,22 +25,28 @@ export default function Container() {
       ) : (
         <div className='min-h-[500px] w-[90%] mx-auto'>
           {postDetail && (
-            <>
-              <Hero />
-
-              <div className='grid grid-cols-12'>
-                <DetailLeft />
-                <DetailRight />
-              </div>
-              <div>Facilities</div>
+            <div className='flex flex-col gap-16'>
               <div>
-                <div>Google Map</div>
-                <div>nearest essentials</div>
+                <Hero />
+
+                <div className='grid grid-cols-12 '>
+                  <DetailLeft />
+                  <DetailRight />
+                </div>
+              </div>
+              <Facilities />
+              <div className='grid grid-cols-12'>
+                <div className='col-span-8 '>
+                  <div className='w-[90%] rounded-md overflow-hidden shadow-md'>
+                    <MapDisplayOnePin zoom={18} marker={latlong} />
+                  </div>
+                </div>
+                <div className='col-span-4 bg-blue-100'>nearest essentials</div>
               </div>
               <ProfileCard user={postDetail?.user} title='Hosted by'>
                 <Button variant='outlined'>Contact Host</Button>
               </ProfileCard>
-            </>
+            </div>
           )}
         </div>
       )}
