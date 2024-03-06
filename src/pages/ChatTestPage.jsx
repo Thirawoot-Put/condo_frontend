a72f77fce8678d3939b66960e84cecfcf962a8d3
chatpimuk-l <chatpimuk2389@gmail.com>
2024-03-06 15:06:08 +0700

    chat v1

---
+++ src/pages/ChatTestPage.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+let socket;
+
+export default function ChatTestPage() {
+  const location = useLocation();
+
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = 'localhost:8080';
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit('join', { name, room }, () => {});
+
+    return () => {
+      socket.emit('disconnect');
+
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit('sendMessage', message, () => {
+        setMessage('');
+      });
+    }
+  };
+
+  console.log(message, messages);
+
+  return (
+    <div className=''>
+      <div className=''>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => (e.key === 'Enter' ? sendMessage(e) : null)}
+          type='text'
+        />
+      </div>
+    </div>
+  );
+}
