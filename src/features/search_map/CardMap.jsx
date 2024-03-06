442ef94ea9b51036bab8539e9ff3f040a1f213d4
Thirawoot-put <thirawoot.put@gmail.com>
2024-03-06 15:58:10 +0700

    ui card map

---
+++ src/features/search_map/CardMap.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { MdOutlineLocationOn } from 'react-icons/md';
+
+function CardMap() {
+  return (
+    <div className='card card-side bg-base-100 shadow-xl p-6 w-[32rem] justify-between gap-4'>
+      <div className='flex h-48 aspect-square overflow-hidden items-center rounded-xl'>
+        <img
+          className='object-center'
+          src='https://images.pexels.com/photos/533157/pexels-photo-533157.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+          alt='pic'
+        />
+      </div>
+
+      <div className='flex flex-col justify-between flex-1'>
+        <div>
+          <h1 className='font-semibold text-xl'>room number</h1>
+          <h1>Condo name</h1>
+          <small className='flex items-center'>
+            <MdOutlineLocationOn />
+            <span>district, province</span>
+          </small>
+          <div>
+            <small className='pr-1 text-gray-500'>utils |</small>
+            <small className='pr-1 text-gray-500'>utils |</small>
+            <small className='pr-1 text-gray-500'>utils |</small>
+          </div>
+        </div>
+        <div className='text-end'>
+          <span className='text-lg font-semibold'>THB 3,000 </span>
+          <span className='text-sm'>/month</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardMap;
--- src/pages/SearchMapPage.jsx
+++ src/pages/SearchMapPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import MapDisplay from '../components/MapDisplay';
 import { markersMap } from '../mock';
+import CardMap from '../features/search_map/CardMap';
 
 function SearchMapPage() {
-  return <div>Hello map</div>;
+  return (
+    <div className='flex justify-center items-center py-10'>
+      <CardMap />
+    </div>
+  );
 }
 
 export default SearchMapPage;
