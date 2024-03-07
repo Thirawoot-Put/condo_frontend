d0590f516a8182907ba0684e25c7a06f98cc02b6
chatpimuk-l <chatpimuk2389@gmail.com>
2024-03-07 16:54:07 +0700

    link to chat

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import AuthContextProvider from './features/auth/context/AuthContext';
 import 'react-toastify/dist/ReactToastify.css';
+import ChatContextProvider from './features/chat/context/ChatContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <AuthContextProvider>
-    <App />
+    <ChatContextProvider>
+      <App />
+    </ChatContextProvider>
   </AuthContextProvider>
   // </React.StrictMode>
 );
