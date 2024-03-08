cdacc08172cae4500e39f100285bfe9d0eaebb44
fee-hanafee <hanafee.pass@gmail.com>
2024-03-08 20:02:53 +0700

    70percent

--- src/features/review/Component/Review.jsx
+++ src/features/review/Component/Review.jsx
@@ -1,53 +1,121 @@
 import { Button, Rating, TextField } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useReview from '../context/ReviewContext';
 
 export default function Review() {
-  
-  const { handleChange, handleSubmit, error, input } = useReview();
+  const { handleChange, handleSubmit, error, input, fetchReviewMe, reviewMe } =
+    useReview();
+  const [isEdit, setIsEdit] = useState(false);
+  const EDIT = !isEdit ? { R1: 'readOnly', R2: 'disabled' } : { R1: '', R2: '' };
+  const [newValue, setNewValue] = useState({
+    ...reviewMe,
+  });
+
+  console.log(reviewMe);
+  useEffect(() => {
+    fetchReviewMe();
+  }, []);
   return (
-    <form
-      onSubmit={handleSubmit}
-      className='min-h-[200px] shadow-md rounded-md w-[40%] p-4 flex flex-col gap-4'
-    >
-      <div>
-        <p>เขียนรีวิว</p>
-      </div>
-      <div>
-        <Rating
-          name='rating'
-          size='large'
-          value={input.rating}
-          onChange={(event, newValue) => {
-            handleChange(event);
-            setValue(newValue);
-          }}
-        />
-        {error.rating && (
-          <p className='text-red-500 text-xs'>Rating is required</p>
-        )}
-      </div>
-      <div className=''>
-        <TextField
-          name='comment'
-          className=''
-          label='แสดงความคิดเห็น'
-          multiline
-          rows={4}
-          fullWidth
-          variant='standard'
-          value={input.comment}
-          onChange={handleChange}
-        />
-        {error.comment && (
-          <p className='text-red-500 text-xs'>Comment is required</p>
-        )}
-      </div>
-      <div>
-        <Button variant='contained' type='submit'>
-          Submit
-        </Button>
-      </div>
-    </form>
+    <>
+      {reviewMe ? (
+        <form
+          onSubmit={handleSubmit}
+          className='min-h-[200px] shadow-md rounded-md p-4 flex flex-col gap-4'
+        >
+          <div>
+            <p>My review</p>
+          </div>
+          <div>
+            <Rating
+              name='rating'
+              size='large'
+              readOnly={EDIT.R1}
+              value={+reviewMe.rating}
+              onChange={(event, newValue) => {
+                handleChange(event);
+              }}
+            />
+            {error.rating && (
+              <p className='text-red-500 text-xs'>Rating is required</p>
+            )}
+          </div>
+          <div className=''>
+            <TextField
+              name='comment'
+              className=''
+              label='แสดงความคิดเห็น'
+              multiline
+              rows={4}
+              fullWidth
+              disabled={EDIT.R2}
+              variant='standard'
+              value={reviewMe.comment}
+              onChange={handleChange}
+            />
+
+            {error.comment && (
+              <p className='text-red-500 text-xs'>Comment is required</p>
+            )}
+          </div>
+          <div>
+            {!isEdit ? (
+              <Button variant='contained' onClick={() => setIsEdit(true)}>
+                Edit
+              </Button>
+            ) : (
+              <div className='flex gap-4'>
+                <Button variant='contained'>Submit</Button>
+                <Button variant='outlined' onClick={() => setIsEdit(false)}>
+                  Cancel
+                </Button>
+              </div>
+            )}
+          </div>
+        </form>
+      ) : (
+        <form
+          onSubmit={handleSubmit}
+          className='min-h-[200px] shadow-md rounded-md p-4 flex flex-col gap-4'
+        >
+          <div>
+            <p>เขียนรีวิว</p>
+          </div>
+          <div>
+            <Rating
+              name='rating'
+              size='large'
+              value={+input.rating}
+              onChange={(event, newValue) => {
+                handleChange(event);
+              }}
+            />
+            {error.rating && (
+              <p className='text-red-500 text-xs'>Rating is required</p>
+            )}
+          </div>
+          <div className=''>
+            <TextField
+              name='comment'
+              className=''
+              label='แสดงความคิดเห็น'
+              multiline
+              rows={4}
+              fullWidth
+              variant='standard'
+              value={input.comment}
+              onChange={handleChange}
+            />
+            {error.comment && (
+              <p className='text-red-500 text-xs'>Comment is required</p>
+            )}
+          </div>
+          <div>
+            <Button variant='contained' type='submit'>
+              Submit
+            </Button>
+          </div>
+        </form>
+      )}
+    </>
   );
 }
--- src/features/review/context/ReviewContext.jsx
+++ src/features/review/context/ReviewContext.jsx
@@ -2,6 +2,7 @@ import { useContext, useState, createContext } from 'react';
 import validateReview from '../../auth/validator/validate-review';
 
 import * as reviewApi from '../../../api/review-api';
+import useAuth from '../../auth/hook/useAuth';
 
 const ReviewContext = createContext();
 
@@ -9,12 +10,26 @@ const initail = { comment: '', rating: 0 };
 
 export function ReviewContextProvider({ children }) {
   const [input, setInput] = useState(initail);
+  const [AllReview, setAllReview] = useState([]);
+  const [reviewMe, setReviewMe] = useState(null)
   const [error, setError] = useState({});
+
+  const { authContext } = useAuth();
 
   const handleChange = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
     setError({ ...error, [e.target.name]: '' });
   };
+
+  const fetchAllReview = async () => {
+    const response = await reviewApi.fetchAllReviews();
+    setAllReview(response.data.reviews)
+    console.log(AllReview)
+  };
+  const fetchReviewMe = async () => {
+    const response = await reviewApi.fetchReviewByUserId()
+    setReviewMe(response.data.review)
+  }
 
   const handleSubmit = async (e) => {
     try {
@@ -33,7 +48,9 @@ export function ReviewContextProvider({ children }) {
   };
 
   return (
-    <ReviewContext.Provider value={{ handleChange, handleSubmit, error,input }}>
+    <ReviewContext.Provider
+      value={{ handleChange, handleSubmit, error, input,fetchAllReview ,AllReview,fetchReviewMe ,reviewMe}}
+    >
       {children}
     </ReviewContext.Provider>
   );
