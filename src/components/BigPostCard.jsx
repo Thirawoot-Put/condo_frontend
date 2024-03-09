66f78003a1545bb98351239c21a7893772fe0f88
Thirawoot-put <thirawoot.put@gmail.com>
2024-03-09 22:58:51 +0700

    layout

--- src/components/BigPostCard.jsx
+++ src/components/BigPostCard.jsx
@@ -9,24 +9,31 @@ function BigPostCard({ data }) {
   };
   console.log(data);
   return (
-    <Link
-      to={`/map?lat=${data.lat}&lng=${data.long}`}
-      className='w-full h-full flex flex-col justify-around items-center'
-    >
-      <div className='h-5/6 w-5/6'>
+    <div className='w-full h-full flex flex-col justify-around items-center'>
+      <Link
+        to={`/map?lat=${data.lat}&lng=${data.long}`}
+        className='h-5/6 w-5/6'
+      >
         <img
           className='rounded-2xl'
           style={Imgstyle}
           src={data.condoImage}
           alt='condo_img'
         />
-      </div>
-      <div className='w-full h-1/6 text-3xl flex justify-around items-center text-black'>
+      </Link>
+      {/* <div className='w-full h-1/6 text-3xl flex justify-around items-center text-black'>
         <div>{data.nameTh}</div>
         <div>{data.nameEn}</div>
+        <div>{data.district.district}</div> */}
+      <Link
+        to={`/map?lat=${data.lat}&lng=${data.long}`}
+        className='w-full h-1/6 text-3xl grid grid-cols-3 items-center text-black'
+      >
+        <div>{data.nameTh}</div>
         <div>{data.district.district}</div>
-      </div>
-    </Link>
+        <div>{data.nameEn}</div>
+      </Link>
+    </div>
   );
 }
 
