f4572905aa358edd3746e5f6ab9d8c26dc6eee4f
CWTWIS <chawanrat.wis@gmail.com>
2024-03-11 13:03:01 +0700

    config footer

--- src/features/footer/MenuList.jsx
+++ src/features/footer/MenuList.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export default function MenuList({ menu , title }) {
-  const navigate = useNavigate()
+export default function MenuList({ menu, title }) {
+  const navigate = useNavigate();
   return (
     <div>
       <div>
         <h1 className='text-xl pb-3 font-semibold'>{title}</h1>
       </div>
-      <div className='w-40'>
+      <div className='flex flex-col'>
         {menu.map((el) => (
           <div key={el.id}>
             <p
